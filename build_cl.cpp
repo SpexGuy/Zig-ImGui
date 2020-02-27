b61bad40fb1f2dec5cfd7863019b318d92b97cf4
Martin Wickham <spexguy070@gmail.com>
2020-02-27 12:09:44 -0600

    Switch to manual CL build to get /MT option

---
+++ build_cl.cpp
@@ -0,0 +1,8 @@
+#define IMGUI_DISABLE_OBSOLETE_FUNCTIONS 1
+#define IMGUI_IMPL_API extern "C" __declspec(dllexport)
+
+#include "imgui/imgui.cpp"
+#include "imgui/imgui_draw.cpp"
+#include "imgui/imgui_demo.cpp"
+#include "imgui/imgui_widgets.cpp"
+#include "cimgui.cpp"
--- build_lib.bat
+++ build_lib.bat
@@ -1,37 +1,4 @@
-rem @echo off
-
-:LUA_GENERATE
-	where luajit || goto NO_LUAJIT
-	where g++ || goto GENERATE_NO_GPP
-
-	pushd "%~dp0\cimgui\generator"
-	luajit .\generator.lua g++
-	popd
-
-	goto AFTER_LUA_GENERATE
-
-
-:GENERATE_NO_GPP
-	echo "Couldn't find g++, make sure MinGW is installed and on your path."
-	echo "Trying to generate with CL instead."
-	where cl || goto GENERATE_NO_CL
-
-	pushd "%~dp0\cimgui\generator"
-	luajit .\generator.lua cl
-	popd
-	goto AFTER_LUA_GENERATE
-
-:GENERATE_NO_CL
-	echo "Couldn't find cl either."
-	echo "Skipping cimgui.cpp generation, using repo data."
-	goto AFTER_LUA_GENERATE
-
-:NO_LUAJIT
-	echo "Couldn't find LuaJIT, make sure it is installed and on your path."
-	echo "Skipping cimgui.cpp generation, using repo data."
-	goto AFTER_LUA_GENERATE
-
-:AFTER_LUA_GENERATE
+:BUILD
 	where cl || goto BUILD_NO_CL
 
 	rd /S /Q "%~dp0\cimgui\build\"
@@ -72,20 +39,10 @@ rem @echo off
 	::copy /B /Y "%~dp0\cimgui\build\Debug\cimgui.lib" "%~dp0\lib\win\cimguid.lib"
 	::copy /B /Y "%~dp0\cimgui\build\Release\cimgui.lib" "%~dp0\lib\win\cimgui.lib"
 
-	goto PYTHON_GENERATE
+	goto DONE
 
 :BUILD_NO_CL
 	echo "Couldn't find cl to build libraries."
-	echo "Skipping library build."
-	goto PYTHON_GENERATE
-
-:PYTHON_GENERATE
-	where python || goto NO_PYTHON
-	"%~dp0\generate.py"
-	goto DONE
-
-:NO_PYTHON
-	echo "Couldn't find python, make sure it is installed and on your path."
 	goto DONE
 
 :DONE
